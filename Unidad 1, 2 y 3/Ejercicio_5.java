d6aafe9c6788888cdd5bda1472af3d381ac3d2af
Carlos-ufpso <cjperezma@ufpso.edu.co>
2024-11-28 13:38:59 -0500

    (revert): Se cambio el nomnbre del archivo

--- Unidad 1, 2 y 3/Ejercicio_5.java
+++
@@ -1,9 +0,0 @@
-public class Ejercicio_5 {
-    public static void main(String[] args) {
-        double cantidad_Inicial = 1000;
-        double retiro_Semanal = 200;
-        int semanas = 4;
-        double cantidad_Final = cantidad_Inicial - (retiro_Semanal * semanas);
-        System.out.println("Cantidad final en la cuenta: $" + cantidad_Final);
-    }
-}
