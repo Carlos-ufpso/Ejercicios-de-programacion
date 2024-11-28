a557c462006dd8a68aee70aaabb8a8e6718a584e
Carlos-ufpso <cjperezma@ufpso.edu.co>
2024-11-28 13:35:28 -0500

    (revert): Se cambio el nomnbre del archivo

---
+++ Unidad 1, 2 y 3/Ejercicio_4_unidad_1.java
@@ -0,0 +1,23 @@
+public class Ejercicio_4_unidad_1 {
+    public static void main(String[] args) 
+    {
+        double precio_Camiseta = 25;
+        double precio_Pantalon = 30;
+
+        double descuento = 0.15; 
+
+        double descuento_Adicional = 0.05; 
+
+        double precio_Camiseta_Descuento = precio_Camiseta * (1 - descuento);
+        double precio_Pantalon_Descuento = precio_Pantalon * (1 - descuento);
+
+        double precio_Total = precio_Camiseta_Descuento + precio_Pantalon_Descuento;
+
+        System.out.println("El precio total con descuento es: $" + precio_Total);
+
+        double precio_Camiseta_Descuento_Adicional = precio_Camiseta_Descuento * (1 - descuento_Adicional) ;
+        double precio_Total_Con_Segunda_Camiseta = precio_Total + precio_Camiseta_Descuento_Adicional;
+
+        System.out.println("El precio total con la segunda camiseta y el descuento adicional es: $" + precio_Total_Con_Segunda_Camiseta);
+    }
+}
