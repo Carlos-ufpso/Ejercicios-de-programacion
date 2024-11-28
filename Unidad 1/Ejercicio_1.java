c5b2ad30d0fd838248b01c239fb1f9f85eb0af14
Carlos-ufpso <cjperezma@ufpso.edu.co>
2024-11-28 09:23:04 -0500

    (revert): Use un scanner cuando no debia entonces ya lo arregle

--- Unidad 1/Ejercicio_1.java
+++ Unidad 1/Ejercicio_1.java
@@ -1,25 +1,14 @@
-import java.util.Scanner;
-
 public class Ejercicio_1 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Ingresa la edad: ");
-        int edad = scanner.nextInt();
-
-        System.out.print("Ingresa la altura (ejemplo: 1,76): ");
-        double altura = scanner.nextDouble();
-
-        System.out.print("Ingresa la inicial del nombre: ");
-        String inicialNombre = scanner.next();
-
+        int edad = 17;
+        double altura = 1.84;
+        String inicialNombre = "C";
         boolean esMayorDeEdad = edad >= 18;
 
-        System.out.println("Edad: " + edad + " años");
-        System.out.println("Altura: " + altura + " metros");
+        System.out.println("Edad: " + edad);
+        System.out.println("Altura: " + altura);
         System.out.println("Inicial del nombre: " + inicialNombre);
         System.out.println("¿Es mayor de edad? " + esMayorDeEdad);
-
-        scanner.close();
     }
 }
+
