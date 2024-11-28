2aa10211a02c8f3fefae4bf907b531d023d71f41
Carlos-ufpso <cjperezma@ufpso.edu.co>
2024-11-28 13:35:53 -0500

    (revert): Se cambio el nomnbre del archivo

---
+++ Unidad 1, 2 y 3/Ejercicio_6_unidad_1.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Ejercicio_6_unidad_1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingresa tu nombre: ");
+        String nombre = scanner.nextLine();
+
+        System.out.print("Ingresa tu edad: ");
+        int edad = scanner.nextInt();
+
+        System.out.print("Ingresa tu altura (en metros): ");
+        double altura = scanner.nextDouble();
+
+        System.out.println("¡Hola, " + nombre + "!");
+        System.out.println("Tienes " + edad + " años y mides " + altura + " metros.");
+
+        scanner.close();
+    }
+}
+
