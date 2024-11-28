0c83594b62518cb6e6f54264384688c4b63600ca
Carlos-ufpso <cjperezma@ufpso.edu.co>
2024-11-28 13:36:38 -0500

    (revert): Se cambio el nomnbre del archivo

---
+++ Unidad 1, 2 y 3/Ejercicio_9_unidad_1.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Ejercicio_9_unidad_1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingresa el primer número: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Ingresa el segundo número: ");
+        int num2 = scanner.nextInt();
+
+        System.out.print("Ingresa el tercer número: ");
+        int num3 = scanner.nextInt();
+
+        if (num1 >= num2 && num1 >= num3) {
+            System.out.println("El primer número es el mayor.");
+        } else if (num2 >= num1 && num2 >= num3) {
+            System.out.println("El segundo número es el mayor.");
+        } else {
+            System.out.println("El tercer número es el mayor.");
+        }
+
+        scanner.close();
+    }
+}
